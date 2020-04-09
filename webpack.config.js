be06462274fe55996ba1a62ad56cc3b8274aa830
JamesVanWaza <jgacuca@gmail.com>
2020-04-09 16:52:39 -0400

    Updated so that, templates can be created

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,3 @@
-// const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-
-// module.exports = {
-//     //	https://webpack.js.org/configuration/mode/
-//     mode: 'development',
-//     entry: {
-//         index: './src/index.js',
-//         algolia: './src/algolia.js',
-//         firebase: './src/firebase.js'
-//     },
-//     output: {
-//         filename: "[name].bundle.js",
-//         //filename: "main.js",
-//         //		path: path.resolve(__dirname, "public") Can change directory name
-//         path: path.resolve(__dirname, "public")
-//     },
-//     // Can change the entry name
-//     // entry: "./mbuzi/mbuzide.js",
-//     devServer: {
-//         contentBase: path.join(__dirname, "public"),
-//         port: 9003
-//     },
-//     optimization: {
-//         //		splitChunks: {
-//         //			chunks: 'all'
-//         //		}
-//         minimizer: [new UglifyJsPlugin()]
-//     },
-//     plugins: [new HtmlWebpackPlugin({
-//         title: "Webpack Cheatsheet",
-//     })],
-//     module: {
-//         rules: [{
-//                 // Whenever a javascript file is found, babel should run and do not compile node_module files
-//                 test: /\.js$/,
-//                 exclude: /(node_modules)/,
-//                 use: {
-//                     loader: 'babel-loader',
-//                     options: {
-//                         // Supports all the browsers
-//                         presets: ['@babel/preset-env', '@babel/preset-react']
-//                     }
-//                 }
-//             },
-//             {
-//                 test: /\.css$/,
-//                 use: [
-//                     { loader: 'style-loader' },
-//                     { loader: 'css-loader' }
-//                 ]
-//             },
-
-// Start here for the URL Loader
-// {
-//     test: /\.(png|jpg)$/,
-//     use: [
-//         { loader: 'url-loader' }
-//     ]
-// }
-//         ]
-//     }
-// }
-
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require("webpack"); // to access built-in plugins
@@ -68,12 +5,12 @@ const webpack = require("webpack"); // to access built-in plugins
 module.exports = {
     mode: 'development',
     entry: {
-        index: './src/index.js',
-        algolia: './src/algolia.js',
-        firebase: './src/firebase.js'
+        index: './js/index.js',
+        algolia: './js/algolia.js',
+        firebase: './js/firebase.js'
     },
     output: {
-        filename: "[name].bundle.js",
+        filename: "[name].js",
         path: path.resolve(__dirname, "public")
     },
     module: {
@@ -104,26 +41,23 @@ module.exports = {
         ]
     },
     plugins: [
-        // Generates default index.html
-        // new HtmlWebpackPlugin({
-        //     title: 'Home',
-        //     filename: 'index.html',
-        //     template: './src/index.ejs'
-        // }),
-
-        // // about.html
-        // new HtmlWebpackPlugin({
-        //     title: 'Algolia',
-        //     filename: 'algolia.html',
-        //     template: './src/algolia.ejs'
-        // }),
-
-        // // contact.html
-        // new HtmlWebpackPlugin({
-        //     title: 'Firebase',
-        //     filename: 'firebase.html',
-        //     template: './src/firebase.ejs'
-        // })
-        new HtmlWebpackPlugin({ template: './src/index-template.html' })
+        // Algolia Page
+        new HtmlWebpackPlugin({
+            title: 'Algolia',
+            filename: 'algolia.html',
+            template: './html-templates/algolia-template.html'
+        }),
+        // Firebase Page
+        new HtmlWebpackPlugin({
+            title: 'Firebase',
+            filename: 'firebase.html',
+            template: './html-templates/firebase-template.html'
+        }),
+        // Home Page
+        new HtmlWebpackPlugin({
+            title: 'Webpack Tutorials',
+            filename: 'index.html',
+            template: './html-templates/index-template.html'
+        })
     ]
 };
