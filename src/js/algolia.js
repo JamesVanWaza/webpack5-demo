e1de938c6f41a9ea43ba77473a028a0401af6eb8
JamesVanWaza <jgacuca@gmail.com>
2020-04-12 15:31:45 -0400

    Removed document.write code

--- src/js/algolia.js
+++ src/js/algolia.js
@@ -1,21 +1,9 @@
 /** Stylesheet **/
 import '../scss/style.scss';
 
-/** Algolia CSS **/
-//import algoliamincss from 'instantsearch.css';
-
-/** HTML in JS **/
-//document.write("<h1>Hello Webpack Demo!</h1>");
-//document.write('<div class="ais-InstantSearch"><h2>InstantSearch.js e-commerce demo</h2><div class="right-panel"><div id="searchbox"></div><div id="hits"></div><div id="pagination"></div></div></div>');
-//document.write("<h6>Attention:: All this code is coming from javascript - that's preety cool.</h6>");
-//document.write('<div id="image"></div>');
-
-//document.write("<h2>Firebase Tutorial</h2><h3 id='hotDogOutput'>Hot dog status:</h3><input type='textfield' id='latestHotDogStatus'><button id='saveButton'>Save</button><button id='loadButton'>Load</button>");
-
 /** Algolia JS **/
 import algoliasearch from 'algoliasearch/lite';
-//import instantsearch from 'instantsearch.js';
-import instantsearchjs from 'instantsearch.js';
+import instantsearch from 'instantsearch.js';
 
 import {
     searchBox,
