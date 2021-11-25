c5370ff970ab918a4fda5f497107f1cff66d8c85
JamesVanWaza <jgacuca@gmail.com>
2021-11-25 13:23:36 -0500

    Setup of Webpack testing following setup from Swashbucking with code

--- webpack.config.js
+++ webpack.config.js
@@ -1,97 +1,40 @@
-const path = require("path");
+const path = require('path');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const webpack = require("webpack"); // to access built-in plugins
-
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-
-const TerserPlugin = require('terser-webpack-plugin');
+const mode = process.env.NODE_ENV || 'production' ? 'production' :
+    'development';
 
 module.exports = {
-    //	https://webpack.js.org/configuration/mode/
-    mode: 'development',
-    entry: {
-        index: './src/js/index.js',
-        algolia: './src/js/algolia.js',
-        firebase: './src/js/firebase.js'
-    },
+    mode: mode,
+
+    entry: './src/index.js',
+
     output: {
-        filename: "[name].bundle.js",
-        //filename: "main.js",
-        //		path: path.resolve(__dirname, "public") Can change directory name
-        path: path.resolve(__dirname, "public")
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
     },
+
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                // without additional configuration, babel-loader will use the .babelrc file
+                loader: "babel-loader"
+            }
+        }]
+    },
+
+    devtool: 'source-map',
+
+    target: 'web',
+
     devServer: {
         open: {
             app: 'Google Chrome'
         },
         watchFiles: {
-            paths: ['src/']
+            paths: ['public/']
         },
-        port: 2011
-    },
-    module: {
-        rules: [{
-                test: /\.txt$/,
-                use: 'raw-loader'
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS Strings
-                    { loader: 'style-loader' },
-
-                    // Translates CSS into CommonJS
-                    { loader: 'css-loader' },
-
-                    // Compiles Sass to CSS
-                    { loader: 'sass-loader' }
-                ]
-            },
-            // Start here for the URL Loader
-            {
-                test: /\.(png|jpg)$/,
-                use: [
-                    { loader: 'url-loader' }
-                ]
-            },
-            {
-                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9]\.png|jpg)?$/,
-                use: 'url-loader?limit=10000',
-            },
-            {
-                test: /\.(ttf|eot|svg|png|jpg|jpeg)(\?[\s\S]+)?$/,
-                use: 'file-loader',
-            }
-        ]
-    },
-    plugins: [
-        // Home Page
-        new HtmlWebpackPlugin({
-            title: 'Responsive Navbar Tutorial',
-            filename: 'index.html',
-            template: './src/html-templates/index-template.html'
-        }),
-        // Algolia Page
-        new HtmlWebpackPlugin({
-            title: 'Algolia Tutorial',
-            filename: 'algolia.html',
-            template: './src/html-templates/algolia-template.html'
-        }),
-        // Firebase Page
-        new HtmlWebpackPlugin({
-            title: 'Firebase Tutorial',
-            filename: 'firebase.html',
-            template: './src/html-templates/firebase-template.html'
-        }),
-        // Favicon
-        new FaviconsWebpackPlugin({
-            logo: './src/images/W-Favicon002@4x.png'
-        })
-    ],
-    optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin()]
+        port: 2014
     }
 };
