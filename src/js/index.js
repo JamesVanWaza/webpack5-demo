9727b7d36b261591d961e6cd40b32c3938673c1d
JamesVanWaza <jgacuca@gmail.com>
2021-11-26 17:19:29 -0500

    Setting up Tailwind: WIP

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+// import getClasses from './getClasses';
+
+// console.log('ran from index.js');
+// getClasses();
+
+// const obj = {
+//     a: 'alpha',
+//     b: 'bravo',
+// };
+
+// const newObj = {
+//     ...obj,
+//     c: 'charlie'
+// };
+
+// console.log(newObj);
+
+/** Stylesheet **/
+import '../scss/style.scss';
+
+const headline = "Welcome to the webpage";
+document.querySelector('h1').innerText = headline;
+
+/** Tailwind CSS **/
+import 'tailwindcss/tailwind.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const mode = process.env.NODE_ENV || 'production' ? 'production' :
@@ -6,22 +7,65 @@ const mode = process.env.NODE_ENV || 'production' ? 'production' :
 module.exports = {
     mode: mode,
 
-    entry: './src/index.js',
+    entry: './src/js/index.js',
 
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
     },
 
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/html-templates/index-template.html'
+        }),
+    ],
+
     module: {
         rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                // without additional configuration, babel-loader will use the .babelrc file
-                loader: "babel-loader"
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    // without additional configuration, babel-loader will use the .babelrc file
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.txt$/,
+                use: 'raw-loader'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS Strings
+                    { loader: 'style-loader' },
+
+                    // Translates CSS into CommonJS
+                    { loader: 'css-loader' },
+
+                    // PostCSS
+                    { loader: 'postcss-loader' },
+
+                    // Compiles Sass to CSS
+                    { loader: 'sass-loader' },
+                ]
+            },
+            // Start here for the URL Loader
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    { loader: 'url-loader' }
+                ]
+            },
+            {
+                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9]\.png|jpg)?$/,
+                use: 'url-loader?limit=10000',
+            },
+            {
+                test: /\.(ttf|eot|svg|png|jpg|jpeg)(\?[\s\S]+)?$/,
+                use: 'file-loader',
             }
-        }]
+        ]
     },
 
     devtool: 'source-map',
