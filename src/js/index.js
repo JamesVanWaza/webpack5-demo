9438133e21f10871fea66ca5ac4b707895190c7b
JamesVanWaza <jgacuca@gmail.com>
2022-02-10 22:43:59 -0500

    Added the footer

--- src/js/index.js
+++ src/js/index.js
@@ -26,3 +26,31 @@ document.querySelector('h1').innerText = headline;
 
 // import mainImg from '../images/trees.jpeg';
 // img.src = mainImg;
+
+/** Footer */
+document.body.onload = footer;
+
+function footer() {
+    // create a new div element
+    const footerDiv = document.createElement("footer");
+
+    // assign it a class
+    footerDiv.classList.add("footer");
+
+    // gets the current date
+    const copyright = new Date().getFullYear();
+
+    // gets the copyright symbol
+    const favicon = document.createElement("i");
+    favicon.classList.add("fa-regular", "fa-copyright");
+
+    const text = document.createTextNode(" 2020 " + "- " + copyright);
+
+    // add the text node to the newly created div
+    footerDiv.appendChild(favicon);
+    footerDiv.appendChild(text);
+
+    // add the newly created element and its content into the DOM
+    const newDiv = document.getElementById("div");
+    document.body.insertBefore(footerDiv, newDiv);
+}
