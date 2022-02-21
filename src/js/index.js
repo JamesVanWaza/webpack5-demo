a7c3c90ac18bc753b1071a1efae40e02cb7857ba
JamesVanWaza <jgacuca@gmail.com>
2022-02-21 00:06:06 -0500

    Removed the headline temporarily

--- src/js/index.js
+++ src/js/index.js
@@ -21,8 +21,8 @@ import '../scss/style.scss';
 /** FontAwesome 6 */
 import '@fortawesome/fontawesome-free/js/all.js';
 
-const headline = "Welcome to the webpage";
-document.querySelector('h1').innerText = headline;
+// const headline = "Welcome to the webpage";
+// document.querySelector('h1').innerText = headline;
 
 // import mainImg from '../images/trees.jpeg';
 // img.src = mainImg;
