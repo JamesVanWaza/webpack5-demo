70974ade23e1d7e4d428b485eba72bf4926bb3a5
JamesVanWaza <jgacuca@gmail.com>
2022-05-22 20:04:26 -0400

    WIP on setting up colors

--- src/js/index.js
+++ src/js/index.js
@@ -44,7 +44,7 @@ function footer() {
     const favicon = document.createElement("i");
     favicon.classList.add("fa-regular", "fa-copyright");
 
-    const text = document.createTextNode(" 2020 " + "- " + copyright);
+    const text = document.createTextNode("2020 " + "- " + copyright);
 
     // add the text node to the newly created div
     footerDiv.appendChild(favicon);
