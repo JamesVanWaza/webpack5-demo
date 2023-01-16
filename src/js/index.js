9dc854faa9f37aa1bca58c4575dce2076a02fa3a
JamesVanWaza <jgacuca@gmail.com>
2023-01-15 19:19:10 -0500

    Deleted the old js files

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,5 @@
-// import getClasses from './getClasses';
-
-// console.log('ran from index.js');
-// getClasses();
-
-// const obj = {
-//     a: 'alpha',
-//     b: 'bravo',
-// };
-
-// const newObj = {
-//     ...obj,
-//     c: 'charlie'
-// };
-
-// console.log(newObj);
-
 /** Stylesheet **/
 import '../scss/style.scss';
 
 const headline = "Welcome to the webpage";
 document.querySelector('h1').innerText = headline;
-
-// import mainImg from '../images/trees.jpeg';
-// img.src = mainImg;
