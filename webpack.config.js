7db582aed3fa243c24ef72dfed061b8b4fe78ece
JamesVanWaza <jgacuca@gmail.com>
2023-01-15 19:23:03 -0500

    Updated to use multiple js pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/js/index.js',
+
+    entry: {
+        index: './src/js/index.js',
+    },
 
     output: {
         filename: 'bundle.js',
