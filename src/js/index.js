20b448a3a4ef0c164cfac594ead8b44ed18c6cad
JamesVanWaza <jgacuca@gmail.com>
2023-04-23 18:49:47 -0400

    Changed the template to match start-sass

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,75 @@
-/** Stylesheet **/
 import '../scss/style.scss';
 
-const headline = "Welcome to the webpage";
-document.querySelector('h1').innerText = headline;
+/** Navigation Menu */
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+const items = document.querySelectorAll(".item");
+
+/** Toggle mobile menu */
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else {
+        menu.classList.add("active");
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+}
+
+/** Activate Submenu */
+function toggleItem() {
+    if (this.classList.contains("submenu-active")) {
+        this.classList.remove("submenu-active");
+    } else if (menu.querySelector(".submenu-active")) {
+        menu.querySelector(".submenu-active").classList.remove("submenu-active");
+        this.classList.add("submenu-active");
+    } else {
+        this.classList.add("submenu-active");
+    }
+}
+
+/** Close Submenu From Anywhere */
+function closeSubmenu(e) {
+    let isClickInside = menu.contains(e.target);
+
+    if (!isClickInside && menu.querySelector(".submenu-active")) {
+        menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    }
+}
+/** Event Listeners */
+toggle.addEventListener("click", toggleMenu, false);
+for (let item of items) {
+    if (item.querySelector(".submenu")) {
+        item.addEventListener("click", toggleItem, false);
+    }
+    item.addEventListener("keypress", toggleItem, false);
+}
+document.addEventListener("click", closeSubmenu, false);
+
+/** Footer */
+document.body.onload = footer;
+
+function footer() {
+    // create a new div element
+    const footerDiv = document.createElement("footer");
+
+    // assign it a class
+    footerDiv.classList.add("footer");
+
+    // gets the current date
+    const copyright = new Date().getFullYear();
+
+    // gets the copyright symbol
+    const favicon = document.createElement("i");
+    favicon.classList.add("fas", "fa-copyright");
+
+    const text = document.createTextNode(" 2020 " + "- " + copyright);
+
+    // add the text node to the newly created div
+    footerDiv.appendChild(favicon);
+    footerDiv.appendChild(text);
+
+    // add the newly created element and its content into the DOM
+    const newDiv = document.getElementById("div");
+    document.body.insertBefore(footerDiv, newDiv);
+}
