4e186b89d90272045d0dee6df5278994ce1149f2
JamesVanWaza <jgacuca@gmail.com>
2023-05-01 17:27:42 -0400

    Updating so images and fonts can use asset/resource still WIP

--- webpack.config.js
+++ webpack.config.js
@@ -62,8 +62,12 @@ module.exports = {
                 ]
             },
             {
-                test: /\.png|jpeg/,
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
             },
         ]
     },
